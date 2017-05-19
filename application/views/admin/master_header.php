0c83687903953ca0b0bf8ed9d8475b29118273d7
Miles Chick <mileschick@gmail.com>
2017-05-19 10:41:28 -0500

    Adding viewport to edit pages

--- application/views/admin/master_header.php
+++ application/views/admin/master_header.php
@@ -8,6 +8,7 @@
     <meta charset="utf-8" />
     <meta name="description" content="">
     <meta name="keywords" content="">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
     
     
     <script src="<?php echo base_url(); ?>js/jquery-3.1.0.min.js"></script>
